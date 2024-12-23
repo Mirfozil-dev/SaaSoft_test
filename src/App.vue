3c405cd855eb3f425a46682f3cb63958edfb1b03
Mirfozil Makhmudov <makhmudovv003@gmail.com>
2024-12-23 21:50:50 +0200

    connected vuetify, made all layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,65 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import AccountsHeader from "@/components/AccountsHeader.vue";
+
+const accountTypes = ['Локальная', 'LDAP'];
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container>
+    <v-card>
+      <accounts-header />
+      <v-card-text>
+        <v-alert type="info" class="mb-4" density="compact">
+          Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+        </v-alert>
+          <v-row>
+            <v-col>
+              <v-textarea
+                  rows="1"
+                  auto-grow
+                  label="Метки"
+                  variant="outlined"
+                  density="compact"
+                  color="primary"
+              ></v-textarea>
+            </v-col>
+            <v-col>
+              <v-select
+                  :items="accountTypes"
+                  label="Тип записи"
+                  variant="outlined"
+                  density="compact"
+                  color="primary"
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-text-field
+                  label="Логин"
+                  variant="outlined"
+                  density="compact"
+                  color="primary"
+                  required
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                  label="Пароль"
+                  variant="outlined"
+                  density="compact"
+                  color="primary"
+                  required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="1" class="d-flex justify-end h-100">
+              <v-btn icon size="small" rounded="lg">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/AccountsHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card-title>
+    Учётные записи
+    <v-btn icon class="ml-2" size="small" rounded="lg" color="success">
+      <v-icon size="large">mdi-plus</v-icon>
+    </v-btn>
+  </v-card-title>
+</template>
+
+<style scoped>
+
+</style>
